28265d8a68bf29a163772dae441ed0c4d7d121c2
lucas-manuel <lucasmanuel.tech@gmail.com>
2023-11-20 10:40:42 -0500

    feat: add aave caase

--- src/SparkLendHealthChecker.sol
+++ src/SparkLendHealthChecker.sol
@@ -10,8 +10,11 @@ import { IPool }             from "lib/aave-v3-core/contracts/interfaces/IPool.s
 
 contract SparkLendHealthChecker {
 
-    address constant DATA_PROVIDER = 0xFc21d6d146E6086B8359705C8b28512a983db0cb;
-    address constant POOL          = 0xC13e21B648A5Ee794902342038FF3aDAB66BE987;
+    // address constant DATA_PROVIDER = 0xFc21d6d146E6086B8359705C8b28512a983db0cb;
+    // address constant POOL          = 0xC13e21B648A5Ee794902342038FF3aDAB66BE987;
+
+    address constant DATA_PROVIDER = 0x7B4EB56E7CD4b454BA8ff71E4518426369a138a3;
+    address constant POOL          = 0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2;
 
     IPool             pool         = IPool(POOL);
     IPoolDataProvider dataProvider = IPoolDataProvider(DATA_PROVIDER);
@@ -23,49 +26,39 @@ contract SparkLendHealthChecker {
     function check1() external view returns (bool) {
         IPoolDataProvider.TokenData[] memory tokenData = dataProvider.getAllReservesTokens();
 
+        console2.log("tokenData.length: %s", tokenData.length);
+
         for (uint256 i = 0; i < tokenData.length; i++) {
-            console.log("----------------------------------------");
-            console.log("tokenData[%s].symbol:       %s", i, tokenData[i].symbol);
-            console.log("tokenData[%s].tokenAddress: %s", i, tokenData[i].tokenAddress);
-
-            (
-                uint256 unbacked,
-                uint256 accruedToTreasuryScaled,
-                uint256 totalAToken,
-                uint256 totalStableDebt,
-                uint256 totalVariableDebt,
-                uint256 liquidityRate,
-                uint256 variableBorrowRate,
-                uint256 stableBorrowRate,
-                uint256 averageStableBorrowRate,
-                uint256 liquidityIndex,
-                uint256 variableBorrowIndex,
-                uint40 lastUpdateTimestamp
-              ) = dataProvider.getReserveData(tokenData[i].tokenAddress);
-
-            console.log("unbacked:                  %s", unbacked);
-            console.log("accruedToTreasuryScaled:   %s", accruedToTreasuryScaled);
-            console.log("totalAToken:               %s", totalAToken);
-            console.log("totalStableDebt:           %s", totalStableDebt);
-            console.log("totalVariableDebt:         %s", totalVariableDebt);
-            console.log("liquidityRate:             %s", liquidityRate);
-            console.log("variableBorrowRate:        %s", variableBorrowRate);
-            console.log("stableBorrowRate:          %s", stableBorrowRate);
-            console.log("averageStableBorrowRate:   %s", averageStableBorrowRate);
-            console.log("liquidityIndex:            %s", liquidityIndex);
-            console.log("variableBorrowIndex:       %s", variableBorrowIndex);
-            console.log("lastUpdateTimestamp:       %s", lastUpdateTimestamp);
-            console.log("----------------------------------------");
-
-            uint256 totalDebt = totalStableDebt + totalVariableDebt;
-
-            console.log("totalDebt:                 %s", totalDebt);
-
-            ( address aToken,, )
-                = dataProvider.getReserveTokensAddresses(tokenData[i].tokenAddress);
-
-            console.log("aTokenBal", IERC20(tokenData[i].tokenAddress).balanceOf(aToken));
+            console2.log("i", i);
+            console2.log("----------------------------------------");
+            console2.log("token:                     %s", tokenData[i].symbol);
+            checkReserveInvariants(tokenData[i].tokenAddress);
         }
+    }
+
+    function checkReserveInvariants(address asset) public view {
+        ( uint256 unbacked, uint256 accruedToTreasuryScaled,,,,,,,,,, )
+            = dataProvider.getReserveData(asset);
+
+        ( address aToken,, ) = dataProvider.getReserveTokensAddresses(asset);
+
+        uint256 totalDebt      = dataProvider.getTotalDebt(asset);
+        uint256 totalLiquidity = IERC20(asset).balanceOf(aToken);
+
+        uint256 assets      = totalLiquidity + totalDebt;
+        uint256 liabilities = IERC20(aToken).totalSupply() + accruedToTreasuryScaled;
+
+        console2.log("totalSupply:               %s", IERC20(aToken).totalSupply());
+        console2.log("unbacked                   %s", unbacked);
+        console2.log("assets:                    %s", assets);
+        console2.log("liabilities:               %s", liabilities);
+        console2.log("assets - liabilities:      %s", assets - liabilities);
+
+        uint256 diff = (assets - liabilities) * 1e18 / 10 ** IERC20(asset).decimals();
+
+        console2.log("diff:                      %s", diff * 10_000 / 1e18);
+
+        console2.log("----------------------------------------");
     }
 
 
@@ -95,6 +88,10 @@ contract SparkLendHealthChecker {
         belowLtv = healthFactor < _bpsToWad(ltv);
 
         belowLiquidationThreshold = healthFactor < _bpsToWad(currentLiquidationThreshold);
+    }
+
+    function _toWad(address asset, uint256 amount) internal view returns (uint256) {
+        return amount * 1e18 / 10 ** IERC20(asset).decimals();
     }
 
     function _bpsToWad(uint256 bps) internal pure returns (uint256) {
