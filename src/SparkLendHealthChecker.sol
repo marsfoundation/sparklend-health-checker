a5f459197f6281c5512b9ea3e84ccb5c3efef966
lucas-manuel <lucasmanuel.tech@gmail.com>
2023-11-20 08:40:31 -0500

    feat: add checkUserHealth function

--- src/SparkLendHealthChecker.sol
+++ src/SparkLendHealthChecker.sol
@@ -1,10 +1,49 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.13;
 
+import { IPoolDataProvider } from "lib/aave-v3-core/contracts/interfaces/IPoolDataProvider.sol";
+import { IPool }             from "lib/aave-v3-core/contracts/interfaces/IPool.sol";
+
 contract SparkLendHealthChecker {
+
+    address constant DATA_PROVIDER = 0xFc21d6d146E6086B8359705C8b28512a983db0cb;
+    address constant POOL          = 0xC13e21B648A5Ee794902342038FF3aDAB66BE987;
+
+    IPool             pool         = IPool(POOL);
+    IPoolDataProvider dataProvider = IPoolDataProvider(DATA_PROVIDER);
 
     function runChecks() public view returns (bool) {
         return true;
+    }
+
+    function checkUserHealth(address user)
+        public view returns (
+            uint256 totalCollateralBase,
+            uint256 totalDebtBase,
+            uint256 availableBorrowsBase,
+            uint256 currentLiquidationThreshold,
+            uint256 ltv,
+            uint256 healthFactor,
+            bool belowLtv,
+            bool belowLiquidationThreshold
+        )
+    {
+        (
+            totalCollateralBase,
+            totalDebtBase,
+            availableBorrowsBase,
+            currentLiquidationThreshold,
+            ltv,
+            healthFactor
+        ) = pool.getUserAccountData(user);
+
+        belowLtv = healthFactor < _bpsToWad(ltv);
+
+        belowLiquidationThreshold = healthFactor < _bpsToWad(currentLiquidationThreshold);
+    }
+
+    function _bpsToWad(uint256 bps) internal pure returns (uint256) {
+        return bps * 1e14;
     }
 
 }
--- test/SparkLendHealthChecker.t.sol
+++ test/SparkLendHealthChecker.t.sol
@@ -9,12 +9,35 @@ contract SparkLendHealthCheckerTest is Test {
 
     SparkLendHealthChecker public healthChecker;
 
+    address constant WHALE = 0xf8dE75c7B95edB6f1E639751318f117663021Cf0;
+
     function setUp() public {
+        vm.createSelectFork(getChain('mainnet').rpcUrl, 18613327);
         healthChecker = new SparkLendHealthChecker();
     }
 
     function test_runChecks() public {
-        assertEq(healthChecker.runChecks(), true);
+        (
+            uint256 totalCollateralBase,
+            uint256 totalDebtBase,
+            uint256 availableBorrowsBase,
+            uint256 currentLiquidationThreshold,
+            uint256 ltv,
+            uint256 healthFactor,
+            bool belowLtv,
+            bool belowLiquidationThreshold
+        ) = healthChecker.checkUserHealth(WHALE);
+
+        // console.log all of them like this console.log("key: %s, value: %s", key, value)
+        console.log("totalCollateralBase:         %s", totalCollateralBase);
+        console.log("totalDebtBase:               %s", totalDebtBase);
+        console.log("availableBorrowsBase:        %s", availableBorrowsBase);
+        console.log("currentLiquidationThreshold: %s", currentLiquidationThreshold);
+        console.log("ltv:                         %s", ltv);
+        console.log("healthFactor:                %s", healthFactor);
+        console.log("belowLtv:                    %s", belowLtv);
+        console.log("belowLiquidationThreshold:   %s", belowLiquidationThreshold);
+
     }
 
 }
